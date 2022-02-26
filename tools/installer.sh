701ef96b0b6ce1ade568b8a94c38a898b6903612
Chanwoo Park <99chanwoo@gmail.com>
2022-02-26 23:07:55 +0900

    FEATURE Installer guide file download

--- tools/installer.sh
+++ tools/installer.sh
@@ -1,7 +1,27 @@
 #!/bin/bash
 
+# Settings
+REPO="https://raw.githubusercontent.com/sailor1493/tenspoon-python/master/"
+GUIDE="files.txt"
+
+
+
 echo "Hello Installer!"
-if [[ $# -eq 1 ]]
+if [[ $# -lt 1 ]]
 then
+  echo "설치 프로그램은 1개 이상의 변수를 받습니다."
+  echo "관리자에게 문의하세요"
+else
   echo "Hello Installer ${1}"
 fi
+
+# Set Path Variables
+return_path=$PWD
+download_path="./downloaded/${1}/testcase"
+
+# Download Guide File
+echo "설치 정보 파일을 다운로드합니다"
+rm -rf $download_path
+mkdir -p $download_path
+cd $download_path
+wget -nv -O $GUIDE "${REPO}${1}/testcase/${GUIDE}"
