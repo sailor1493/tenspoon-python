7c8008c41939b92b871e11cc13ffaa6235ecad59
sailor1493 <99chanwoo@gmail.com>
2022-02-26 22:07:34 +0900

    RENAME Demo Case

---
+++ demo/answer.py
@@ -0,0 +1,16 @@
+strA = input()
+strB = input()
+
+a = int(strA)
+b = int(strB)
+
+#For Simple Operations: 
+#You may process in the function argument section
+#This is not recommended for expressions with many operators
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(a % b)
+print(a ** b)
