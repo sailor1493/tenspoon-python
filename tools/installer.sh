b171b1c4eaeea77435cb257301ecf8e20d8ce583
Chanwoo Park <99chanwoo@gmail.com>
2022-02-26 23:01:09 +0900

    Demo filelist (#2)
    * RENAME Demo Case
    
    * ADD testcase download guide file

--- tools/installer.sh
+++ tools/installer.sh
@@ -1,7 +1,29 @@
 #!/bin/bash
 
-echo "Hello Installer!"
-if [[ $# -eq 1 ]]
+repo="https://raw.githubusercontent.com/sailor1493/tenspoon-python/master/"
+guide="files.txt"
+
+# Main Routine
+if [[ $# -lt 1 ]]
 then
+  echo "설치 프로그램은 1개 이상의 변수를 입력 받습니다."
+  echo "개발 담당에게 문의하세요"
+  exit 1
+else
   echo "Hello Installer ${1}"
 fi
+
+# move
+echo $PWD
+return_dir=$PWD
+target_dir="./downloaded/${1}/testcase"
+
+# Setup for download: change working directory and download a guide
+rm -rf $target_dir
+mkdir -p $target_dir
+cd $target_dir
+wget -O $guide "${repo}${1}/testcase/${guide}"
+
+# Iteratively download files directed by guide file
+cd $return_dir
+echo $PWD
