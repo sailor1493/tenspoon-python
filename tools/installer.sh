51aab923d4fb50e356b4e05d722e88ec12cbfe13
Chanwoo Park <99chanwoo@gmail.com>
2022-02-28 13:13:47 +0900

    Restore installer.sh

--- tools/installer.sh
+++ tools/installer.sh
@@ -4,23 +4,39 @@
 REPO="https://raw.githubusercontent.com/sailor1493/tenspoon-python/master"
 TARBALL="tarball.tar"
 
-
+# Command Check
 if [[ $# -lt 1 ]]
 then
   echo "설치 프로그램은 1개 이상의 변수를 받습니다."
   echo "관리자에게 문의하세요"
-repo="https://raw.githubusercontent.com/sailor1493/tenspoon-python/master/"
-guide="files.txt"
+  exit 1
+fi
 
-# Main Routine
-if [[ $# -lt 1 ]]
+command=$1
+
+if [[ $command = "grader" ]]
 then
-  echo "설치 프로그램은 1개 이상의 변수를 입력 받습니다."
-  echo "개발 담당에게 문의하세요"
+  echo "채점 프로그램 설치 기능은 아직 구현되지 않았습니다."
+  exit 1
+fi
+
+if [[ $command != "task" ]]
+then
+  echo "지원하지 않는 명령어입니다."
+  echo "관리자에게 문의하세요."
   exit 1
 fi
 
 # Set Variables
 task=$2
 return_path=$PWD
-download_path="./downloaded/${1}/testcase"
+download_path="./downloaded/$task/testcase"
+task_repo="${REPO}/${task}"
+tarball_repo="${task_repo}/testcase/${TARBALL}"
+tarball_local="${download_path}/${TARBALL}"
+
+rm -rf $download_path
+mkdir -p $download_path
+wget -nv -O $tarball_local $tarball_repo
+tar xf $tarball_local -C $download_path
+rm -f $tarball_local
