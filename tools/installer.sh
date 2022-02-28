4bfc538fcc861f3359fa9ae1f35b7807ff7fcd97
Chanwoo Park <99chanwoo@gmail.com>
2022-02-28 13:17:33 +0900

    Devel installer (#4)
    * RENAME Demo Case
    
    * FEATURE Installer guide file download
    
    * Tar testcases
    
    * Installer functionality working
    
    * Grader: iteration structure made
    
    * Restore installer.sh

--- tools/installer.sh
+++ tools/installer.sh
@@ -1,28 +1,40 @@
 #!/bin/bash
 
 # Settings
-REPO="https://raw.githubusercontent.com/sailor1493/tenspoon-python/master/"
-GUIDE="files.txt"
+REPO="https://raw.githubusercontent.com/sailor1493/tenspoon-python/master"
+TARBALL="tarball.tar"
 
-
-
-echo "Hello Installer!"
+# Command Check
 if [[ $# -lt 1 ]]
 then
   echo "설치 프로그램은 1개 이상의 변수를 받습니다."
   echo "관리자에게 문의하세요"
-=======
-repo="https://raw.githubusercontent.com/sailor1493/tenspoon-python/master/"
-guide="files.txt"
-
-# Main Routine
-if [[ $# -lt 1 ]]
+  exit 1
+fi
+command=$1
+if [[ $command = "grader" ]]
 then
-  echo "설치 프로그램은 1개 이상의 변수를 입력 받습니다."
-  echo "개발 담당에게 문의하세요"
+  echo "채점 프로그램 설치 기능은 아직 구현되지 않았습니다."
+  exit 1
+fi
+if [[ $command != "task" ]]
+then
+  echo "지원하지 않는 명령어입니다."
+  echo "관리자에게 문의하세요."
   exit 1
 fi
 
-# Set Path Variables
+# Set Variables
+task=$2
 return_path=$PWD
-download_path="./downloaded/${1}/testcase"
+download_path="./downloaded/$task/testcase"
+task_repo="${REPO}/${task}"
+tarball_repo="${task_repo}/testcase/${TARBALL}"
+tarball_local="${download_path}/${TARBALL}"
+
+# Download Testcases
+rm -rf $download_path
+mkdir -p $download_path
+wget -nv -O $tarball_local $tarball_repo
+tar xf $tarball_local -C $download_path
+rm -f $tarball_local
