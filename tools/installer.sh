8a96c25cc9e9a2be94c0e2437a94091a79ba3276
Chanwoo Park <99chanwoo@gmail.com>
2022-02-28 12:20:52 +0900

    Devel installer (#3)
    * RENAME Demo Case
    
    * FEATURE Installer guide file download
    
    * Tar testcases

--- tools/installer.sh
+++ tools/installer.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+# Settings
+REPO="https://raw.githubusercontent.com/sailor1493/tenspoon-python/master/"
+GUIDE="files.txt"
+
+
+
+echo "Hello Installer!"
+if [[ $# -lt 1 ]]
+then
+  echo "설치 프로그램은 1개 이상의 변수를 받습니다."
+  echo "관리자에게 문의하세요"
+=======
 repo="https://raw.githubusercontent.com/sailor1493/tenspoon-python/master/"
 guide="files.txt"
 
@@ -9,21 +21,8 @@ then
   echo "설치 프로그램은 1개 이상의 변수를 입력 받습니다."
   echo "개발 담당에게 문의하세요"
   exit 1
-else
-  echo "Hello Installer ${1}"
 fi
 
-# move
-echo $PWD
-return_dir=$PWD
-target_dir="./downloaded/${1}/testcase"
-
-# Setup for download: change working directory and download a guide
-rm -rf $target_dir
-mkdir -p $target_dir
-cd $target_dir
-wget -O $guide "${repo}${1}/testcase/${guide}"
-
-# Iteratively download files directed by guide file
-cd $return_dir
-echo $PWD
+# Set Path Variables
+return_path=$PWD
+download_path="./downloaded/${1}/testcase"
