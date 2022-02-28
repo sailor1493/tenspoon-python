4cce151f4ec51e195fb218df53ae1ea92c9d26f6
Chanwoo Park <99chanwoo@gmail.com>
2022-02-28 23:52:37 +0900

    Dev tools (#7)
    * Refactor output_directory
    
    * Ignore Custom-Defined ignore
    
    * Grading Success Case Handled
    
    * Error handler: graded file, test case detection
    
    * Comment
    
    * FEATURE usage help
    
    * FEATURE grade script downloading

--- tools/installer.sh
+++ tools/installer.sh
@@ -3,25 +3,40 @@
 # Settings
 REPO="https://raw.githubusercontent.com/sailor1493/tenspoon-python/master"
 TARBALL="tarball.tar"
+GRADER="grade.sh"
 
 # Command Check
 if [[ $# -lt 1 ]]
 then
   echo "설치 프로그램은 1개 이상의 변수를 받습니다."
   echo "관리자에게 문의하세요"
+  echo "사용법 예시:"
+  echo "  installer.sh task <과제명>"
+  echo "  installer.sh grade"
   exit 1
 fi
 command=$1
-if [[ $command = "grader" ]]
+if [[ $command = "grade" ]]
 then
-  echo "채점 프로그램 설치 기능은 아직 구현되지 않았습니다."
+  echo "채점 프로그램 다운로드를 시작합니다."
+  grade_repo="${REPO}/tools/${GRADER}"
+  grade_local="./tools/${GRADER}"
+  rm -f $grade_local
+  wget -nv -O $grade_local $grade_repo
+  echo "채점 프로그램이 다운로드되었습니다."
   exit 1
 fi
 if [[ $command != "task" ]]
 then
   echo "지원하지 않는 명령어입니다."
+  echo "지원하는 명령어: task, grade"
   echo "관리자에게 문의하세요."
   exit 1
+fi
+if [[ $# -ne 2]]
+then
+  echo "task 명령어는 2개의 명령인자를 받습니다."
+  echo "사용법: installer.sh task <과제명>"
 fi
 
 # Set Variables
